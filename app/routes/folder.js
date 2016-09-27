f87d0edbf4c0efc950ee04768eec4aeefa24bbd6
Stephanie Riera <stephanie@frontside.io>
2016-09-27 11:28:05 -0500

    Create mail lookup service and delete email

--- app/routes/folder.js
+++ app/routes/folder.js
@@ -1,30 +1,10 @@
 import Ember from 'ember';
-import moment from 'moment';
-
-const fixtures = [{
-  id: 1,
-  from:'recruiter@startup.com',
-  to:'dev@me.com',
-  subject:'Opportunity',
-  body:'Foo Body',
-  sentAt:moment(new Date()).subtract(2, 'day').toDate(),
-  readDate: null
-}, {
-  id: 2,
-  from:'ceo@startup.com',
-  to:'dev@me.com',
-  subject:'Wassup',
-  body:'Bruh',
-  sentAt:moment(new Date()).subtract(2, 'minute').toDate(),
-  readDate: null
-}];
 
 const { get } = Ember;
 
 export default Ember.Route.extend({
-  model() {
-    return fixtures.filter(function(item) {
-      return !get(item, 'trashedDate');
-    });
+  mailLookup: Ember.inject.service(),
+  model(params) {
+    return get(this, 'mailLookup').retrieve(params.folderName);
   }
 });
